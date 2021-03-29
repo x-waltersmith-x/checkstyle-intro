8c981e1816f9fe736490fc923b312b50286d8cce
Sofasmile <sofiya.dyakonova@ukr.net>
2021-03-29 10:06:41 +0300

    update task: initialized all fields

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
+
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Hello_World
@@ -29,5 +31,16 @@ public class Hello_World
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
         " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(HELLO);
+    }
+
+    private void initializeVariables() {
+        a = "Hello mates!";
+        System.out.println(a);
+        vARIaBLE = LocalDate.now().getYear();
+        System.out.println("It is " + vARIaBLE + "year");
+        age = 25;
+        System.out.println("I'm " + age);
+        size = 42;
+        System.out.println("The size is " + size);
     }
 }
