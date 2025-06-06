12006a16b82773d97a27527f75d1e5111504588e
x-waltersmith-x <vk.iblogic@gmail.com>
2025-06-06 19:22:00 +0200

    Trying to fix unresolved problems again

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -2,14 +2,13 @@ package core.basesyntax;
 
 import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
-
 import java.time.LocalDate;
 
 public class Hello_World {
-    private String a = "";
-    private int variable = 0;
-    private int age = 0;
-    private int size = 0;
+    private String welcomeMessage;
+    private int currentYear;
+    private int age;
+    private int size;
 
 
     public static void main(String[] args) {
@@ -18,28 +17,35 @@ public class Hello_World {
     }
 
     private String getFromOneToTen() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                result = result + i + " ";
+                result.append(i).append(" ");
             }
         }
-        return result;
+        return result.toString();
     }
 
+    private static final String greetings = "Hello, Mate! I am happy to see you! "
+            + "This class looks weird! Your task is to fix the checkstyle in this file. "
+            + "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message";
+
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file. Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(greetings);
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        welcomeMessage = "Hello mates!";
+        System.out.println(welcomeMessage);
+
+        currentYear = LocalDate.now().getYear();
+        System.out.println("It is " + currentYear + "year");
+
         age = 25;
         System.out.println("I'm " + age);
+
         size = 42;
         System.out.println("The size is " + size);
     }
